82a374f6cca203c76a923d889583050bf2ed743d
talaxasi <kiriesk2@mail.ru>
2021-03-16 12:26:51 +0300

    Black and white fields

--- src/components/Chess/Chess.jsx
+++ src/components/Chess/Chess.jsx
@@ -48,122 +48,175 @@ const figuresList = {
   rookBlack: {
     path: rookBlack,
     name: 'rookBlack',
-    position: [{x: 0, y: 7}, {x: 7, y: 7}]
+    position: [{x: 0, y: 0}, {x: 7, y: 0}]
   },
   knightBlack: {
     path: knightBlack,
     name: 'knightBlack',
-    position: [{x: 1, y: 7}, {x: 6, y: 7}]
+    position: [{x: 1, y: 0}, {x: 6, y: 0}]
   },
   bishopBlack: {
     path: bishopBlack,
     name: 'bishopBlack',
-    position: [{x: 2, y: 7}, {x: 5, y: 7}]
+    position: [{x: 2, y: 0}, {x: 5, y: 0}]
   },
   queenBlack: {
     path: queenBlack,
     name: 'queenBlack',
-    position: [{x: 3, y: 7}]
+    position: [{x: 3, y: 0}]
   },
   kingBlack: {
     path: kingBlack,
     name: 'kingBlack',
-    position: [{x: 4, y: 7}]
+    position: [{x: 4, y: 0}]
   },
   pawnBlack: {
     path: pawnBlack,
     name: 'pawnBlack',
-    position: new Array(8).fill({}).map((el, i) => ({x: i, y: 6})),
+    position: new Array(8).fill({}).map((el, i) => ({x: i, y: 1})),
     move: pawn
   }
 }
 
-const checkCell = (node, comparableFigureColor) => {
-  const {name, x, y} = node.dataset;
-  const currentFigureColor = name.includes('White');
-  if (name === 'cell') return {move: 'move', position: x + y};
-  if (name !== 'cell' && comparableFigureColor !== currentFigureColor) return {move: 'attack', position: x + y};
-  return null;
-}
-
-function pawn(targetDataset, nodeList) {
-  let {name, x, y, id, firstmove} = targetDataset;
-  x = +x;
-  y = +y;
-  const figureWhite = name.includes('White');
+function pawn(name, x, y, figureWhite, nodeList) {
   let canMove = {};
-  if (firstmove) {
-    for (let i = 1; i < 3; i++) {
-      let node = nodeList[y + i][x];
-      let move = checkCell(node, figureWhite);
-      if (move !== null) canMove[move.position] = move;
-    }
+  if (y === 1) {
+    let node = nodeList[y + 2][x];
+    let move = checkCell(node, figureWhite, x, y + 2);
+    let cellPosition = nodeList[y + 2][x].dataset.position;
+    if (move.move === 'move' && move.position !== cellPosition) move = {};
+    if (Object.keys(move).length > 0) canMove[move.position] = move;
   }
+    for (let i = x - 1; i < x + 2; i++) {
+      if (i < 0) i = 0;
+      if (i > 7) break;
+      let node = nodeList[y + 1][i];
+      let move = checkCell(node, figureWhite, i, y + 1);
+      let cellPosition = nodeList[y + 1][x].dataset.position;
+      if (move.move === 'move' && move.position !== cellPosition) move = {};
+      if (move.move === 'attack' && move.position === cellPosition) move = {};
+      if (Object.keys(move).length > 0) canMove[move.position] = move;
+    }
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
   return canMove;
 }
+
+// function rook(name, x, y, figureWhite, nodeList) {
+//   let canMove = {};
+//   for (let i = 0; i < 8; i ++) {
+//
+//   }
+// }
+
+const checkCell = (node, comparableFigureColor, x, y) => {
+  const {position} = node.dataset;
+  const isFigure = node.innerHTML !== '';
+  const currentFigureColor = node.childNodes.length > 0 ? node.childNodes[0].children[0].dataset.name.includes('White') : null;
+  if (!isFigure) return {move: 'move', position, x, y};
+  if (isFigure && comparableFigureColor !== currentFigureColor) return {move: 'attack', position, x: x, y: y};
+  if (isFigure && comparableFigureColor === currentFigureColor) return {move: null}
+  return {};
+}
+
 
 const Chess = () => {
   const [figures, setFigures] = useState(figuresList);
   const [nodeCells, setNodeCells] = useState([]);
   const [nodeCellsReverse, setNodeCellsReverse] = useState([]);
   const [currentFigure, setCurrentFigure] = useState({});
-  const [whiteMove, setWhiteMove] = useState(false);
+  const [whiteMove, setWhiteMove] = useState(true);
   const deskRef = useRef();
 
   const initDesk = new Array(8).fill(0).map(() => new Array(8).fill(0));
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-  const figure = (obj, i) => {
+  const figure = (obj, i, position) => {
     return `<div class="figure">
             <img class="figure__img" src="${obj.path}" alt="x" 
-            data-name="${obj.name}" data-id="${i}" data-firstmove="${true}" 
-            data-x="${obj.position[i].x}" data-y="${obj.position[i].y}"/>
+            data-name="${obj.name}" data-id="${i}" 
+            data-x="${obj.position[i].x}" data-y="${obj.position[i].y}" data-position="${position}"/>
             </div>`
   }
 
   const check = e => {
-    const {name, x, y, id} = e.target.dataset;
-    console.log(x + y)
+    const {name, x, y, id, position} = e.target.dataset;
     const isCurrentFigure = Object.keys(currentFigure).length > 0;
-    if (name !== 'cell' && !isCurrentFigure) {
-      const nodeList = whiteMove ? nodeCells : nodeCellsReverse;
-      const canMove = figures[name].move(e.target.dataset, nodeList);
-      setCurrentFigure({name, id, canMove, position: {x: x, y: y, position: x + y}});
+    const nodeList = whiteMove ? nodeCells : nodeCellsReverse;
+    const reverseNodeList = !whiteMove ? nodeCells : nodeCellsReverse;
+      const whiteFigure = name.includes('White') || false;
+      if (name !== 'cell' && !isCurrentFigure && whiteFigure === whiteMove) {
+        const figureWhite = name.includes('White');
+        const canMove = figures[name].move(name, parseInt(x), parseInt(y), figureWhite, nodeList);
+        if (canMove.move === null) {
+          setCurrentFigure({});
+          return null
+        }
+        setCurrentFigure({name, id, canMove, position: {x: x, y: y, position}});
+      }
+      if (isCurrentFigure && (position in currentFigure.canMove)) {
+        const action = currentFigure.canMove[position].move;
+        const {name: figureName, id: figureId, position: {x: figureX, y: figureY}, canMove} = currentFigure;
+        const move = canMove[position];
+        let newPosition = {...figures[figureName]};
+        newPosition.position[+figureId] = {x: move.x, y: move.y};
+        if (action === 'move') {
+          setFigures({...figures, newPosition});
+          nodeList[+figureY][+figureX].innerHTML = '';
+          setWhiteMove(!whiteMove);
+          setCurrentFigure({});
+        }
+        if (action === 'attack') {
+          const {name, id} = reverseNodeList[y][x].children[0].children[0].dataset;
+          let attackedPosition = {...figures[name]};
+          attackedPosition.position[id] = {x: null, y: null};
+          setFigures({...figures, newPosition, attackedPosition});
+          nodeList[+figureY][+figureX].innerHTML = '';
+          setWhiteMove(!whiteMove);
+          setCurrentFigure({});
+        }
     }
   }
 
   useEffect(() => {
-      const cells = deskRef.current.childNodes;
-      const arr = [...initDesk].map(el => [...el]);
-      let column = 0;
-      let row = 0;
-      for (let i = 0; i < cells.length; i++) {
-        arr[column][row] = cells[i];
-        row++;
-        if (row === 8) {
-          column++;
-          row = 0;
-        }
+    const cells = deskRef.current.childNodes;
+    const arr = [...initDesk].map(el => [...el]);
+    let column = 0;
+    let row = 0;
+    for (let i = 0; i < cells.length; i++) {
+      arr[column][row] = cells[i];
+      row++;
+      if (row === 8) {
+        column++;
+        row = 0;
       }
-      setNodeCellsReverse([...arr]);
-      arr.reverse();
-      setNodeCells([...arr]);
+    }
+    setNodeCellsReverse([...arr]);
+    arr.reverse();
+    setNodeCells([...arr]);
   }, [])
 
   useEffect(() => {
     if (nodeCells.length > 0) {
       for (let key in figures) {
         figures[key].position.forEach((el, i) => {
-          nodeCells[el.y][el.x].innerHTML = figure(figures[key], i)
+          if (el.y !== null && el.x !== null) {
+            if (figures[key].name.includes('White')) {
+              const pos = nodeCells[el.y][el.x].dataset.position;
+              nodeCells[el.y][el.x].innerHTML = figure(figures[key], i, pos);
+            } else {
+              const pos = nodeCellsReverse[el.y][el.x].dataset.position;
+              nodeCellsReverse[el.y][el.x].innerHTML = figure(figures[key], i, pos);
+            }
+          }
         })
       }
     }
-  }, [nodeCells, figures])
+  }, [nodeCellsReverse, nodeCells, figures])
 
   return (
       <div className={'Chess'}>
-        {console.log(currentFigure)}
+        {console.log(currentFigure.canMove)}
         <div className={'desk-border'}>
           <div className={'border-numbers'}>
             {numbers.map(num => <div className={'border-numbers__item'} key={num + '0'}>
@@ -179,17 +232,21 @@ const Chess = () => {
             {initDesk.map((arr, i) => {
               if (i % 2 === 0) {
                 return arr.map((cell, j) => {
+                  const position = `${j}${7 - i}`;
                   if (j % 2 === 0) return <div className={'desk__cell'} data-y={7 - i} data-x={j}
-                                               data-letter={letters[j]} data-name={'cell'} key={j}/>
+                                               data-letter={letters[j]} data-name={'cell'} key={j}
+                                               data-position={position}/>
                   return <div className={'desk__cell desk__cell_brown'} data-y={7 - i} data-x={j}
-                              data-letter={letters[j]} data-name={'cell'} key={j}/>
+                              data-letter={letters[j]} data-name={'cell'} key={j} data-position={position}/>
                 })
               }
               return arr.map((cell, j) => {
+                const position = `${j}${7 - i}`;
                 if (j % 2 === 0) return <div className={'desk__cell desk__cell_brown'} data-y={7 - i} data-x={j}
-                                             data-letter={letters[j]} data-name={'cell'} key={j}/>
+                                             data-letter={letters[j]} data-name={'cell'} key={j}
+                                             data-position={position}/>
                 return <div className={'desk__cell'} data-y={7 - i} data-x={j}
-                            data-letter={letters[j]} data-name={'cell'} key={j}/>
+                            data-letter={letters[j]} data-name={'cell'} key={j} data-position={position}/>
               })
             })}
           </div>
