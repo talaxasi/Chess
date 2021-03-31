93af9e770d26209bb59c8511313fb817528e23c8
talaxasi <kiriesk2@mail.ru>
2021-03-31 17:42:45 +0300

    Done

--- src/components/Chess/Chess.jsx
+++ src/components/Chess/Chess.jsx
@@ -13,80 +13,93 @@ import pawnBlack from '../../icons/pawnBlack.png';
 import queenBlack from '../../icons/queenBlack.png';
 import rookBlack from '../../icons/rookBlack.png';
 
-const figuresList = {
-  rookWhite: {
-    path: rookWhite,
-    name: 'rookWhite',
-    position: [{x: 0, y: 0}, {x: 7, y: 0}]
-  },
-  knightWhite: {
-    path: knightWhite,
-    name: 'knightWhite',
-    position: [{x: 1, y: 0}, {x: 6, y: 0}]
-  },
-  bishopWhite: {
-    path: bishopWhite,
-    name: 'bishopWhite',
-    position: [{x: 2, y: 0}, {x: 5, y: 0}]
-  },
-  queenWhite: {
-    path: queenWhite,
-    name: 'queenWhite',
-    position: [{x: 3, y: 0}]
-  },
-  kingWhite: {
-    path: kingWhite,
-    name: 'kingWhite',
-    position: [{x: 4, y: 0}]
-  },
-  pawnWhite: {
-    path: pawnWhite,
-    name: 'pawnWhite',
-    position: new Array(8).fill({}).map((el, i) => ({x: i, y: 1})),
-    move: pawn
-  },
-  rookBlack: {
-    path: rookBlack,
-    name: 'rookBlack',
-    position: [{x: 0, y: 0}, {x: 7, y: 0}]
-  },
-  knightBlack: {
-    path: knightBlack,
-    name: 'knightBlack',
-    position: [{x: 1, y: 0}, {x: 6, y: 0}]
-  },
-  bishopBlack: {
-    path: bishopBlack,
-    name: 'bishopBlack',
-    position: [{x: 2, y: 0}, {x: 5, y: 0}]
-  },
-  queenBlack: {
-    path: queenBlack,
-    name: 'queenBlack',
-    position: [{x: 3, y: 0}]
-  },
-  kingBlack: {
-    path: kingBlack,
-    name: 'kingBlack',
-    position: [{x: 4, y: 0}]
-  },
-  pawnBlack: {
-    path: pawnBlack,
-    name: 'pawnBlack',
-    position: new Array(8).fill({}).map((el, i) => ({x: i, y: 1})),
-    move: pawn
-  }
-}
 
-function pawn(name, x, y, figureWhite, nodeList) {
-  let canMove = {};
-  if (y === 1) {
-    let node = nodeList[y + 2][x];
-    let move = checkCell(node, figureWhite, x, y + 2);
-    let cellPosition = nodeList[y + 2][x].dataset.position;
-    if (move.move === 'move' && move.position !== cellPosition) move = {};
-    if (Object.keys(move).length > 0) canMove[move.position] = move;
+const Chess = () => {
+
+  const figuresList = {
+    rookWhite: {
+      path: rookWhite,
+      name: 'rookWhite',
+      position: [{x: 0, y: 0}, {x: 7, y: 0}],
+      move: rook
+    },
+    knightWhite: {
+      path: knightWhite,
+      name: 'knightWhite',
+      position: [{x: 1, y: 0}, {x: 6, y: 0}],
+      move: knight
+    },
+    bishopWhite: {
+      path: bishopWhite,
+      name: 'bishopWhite',
+      position: [{x: 2, y: 0}, {x: 5, y: 0}],
+      move: bishop
+    },
+    queenWhite: {
+      path: queenWhite,
+      name: 'queenWhite',
+      position: [{x: 3, y: 0}],
+      move: queen
+    },
+    kingWhite: {
+      path: kingWhite,
+      name: 'kingWhite',
+      position: [{x: 4, y: 0}],
+      move: king
+    },
+    pawnWhite: {
+      path: pawnWhite,
+      name: 'pawnWhite',
+      position: new Array(8).fill({}).map((el, i) => ({x: i, y: 1})),
+      move: pawn
+    },
+    rookBlack: {
+      path: rookBlack,
+      name: 'rookBlack',
+      position: [{x: 0, y: 0}, {x: 7, y: 0}],
+      move: rook
+    },
+    knightBlack: {
+      path: knightBlack,
+      name: 'knightBlack',
+      position: [{x: 1, y: 0}, {x: 6, y: 0}],
+      move: knight
+    },
+    bishopBlack: {
+      path: bishopBlack,
+      name: 'bishopBlack',
+      position: [{x: 2, y: 0}, {x: 5, y: 0}],
+      move: bishop
+    },
+    queenBlack: {
+      path: queenBlack,
+      name: 'queenBlack',
+      position: [{x: 3, y: 0}],
+      move: queen
+    },
+    kingBlack: {
+      path: kingBlack,
+      name: 'kingBlack',
+      position: [{x: 4, y: 0}],
+      move: king
+    },
+    pawnBlack: {
+      path: pawnBlack,
+      name: 'pawnBlack',
+      position: new Array(8).fill({}).map((el, i) => ({x: i, y: 1})),
+      move: pawn
+    }
   }
+
+  function pawn(name, x, y, figureWhite, nodeList) {
+    let canMove = {};
+    if (y === 1) {
+      let node = nodeList[y + 2][x];
+      let move = checkCell(node, figureWhite, x, y + 2);
+      let cellPosition = nodeList[y + 2][x].dataset.position;
+      if (move.move === 'move' && move.position !== cellPosition) move = {};
+      if (Object.keys(move).length > 0) canMove[move.position] = move;
+    }
     for (let i = x - 1; i < x + 2; i++) {
       if (i < 0) i = 0;
       if (i > 7) break;
@@ -98,28 +111,168 @@ function pawn(name, x, y, figureWhite, nodeList) {
       if (Object.keys(move).length > 0) canMove[move.position] = move;
     }
     if (Object.keys(canMove) < 1) return {move: null, position: null};
-  return canMove;
-}
+    return canMove;
+  }
 
-// function rook(name, x, y, figureWhite, nodeList) {
-//   let canMove = {};
-//   for (let i = 0; i < 8; i ++) {
-//
-//   }
-// }
+  function queen(name, x, y, figureWhite, nodeList, figuresList) {
+    let canMove = {};
+    const bishop = figuresList.bishopWhite.move(name, x, y, figureWhite, nodeList);
+    const rook = figuresList.rookWhite.move(name, x, y, figureWhite, nodeList);
+    if (bishop.move !== null) canMove = {...canMove, ...bishop};
+    if (rook.move !== null) canMove = {...canMove, ...rook};
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
+    return canMove;
+  }
 
-const checkCell = (node, comparableFigureColor, x, y) => {
-  const {position} = node.dataset;
-  const isFigure = node.innerHTML !== '';
-  const currentFigureColor = node.childNodes.length > 0 ? node.childNodes[0].children[0].dataset.name.includes('White') : null;
-  if (!isFigure) return {move: 'move', position, x, y};
-  if (isFigure && comparableFigureColor !== currentFigureColor) return {move: 'attack', position, x: x, y: y};
-  if (isFigure && comparableFigureColor === currentFigureColor) return {move: null}
-  return {};
-}
+  function rook(name, x, y, figureWhite, nodeList) {
+    let canMove = {};
+    for (let i = x + 1; i < 8; i ++) {
+      if (isField(i) === null) break;
+      let node = nodeList[y][i];
+      let move = checkCell(node, figureWhite, i, y);
+      if (isField(i, node, move, canMove) === null) break;
+      canMove[move.position] = move;
+    }
+    for (let i = x - 1; i < x; i --) {
+      if (isField(i) === null) break;
+      let node = nodeList[y][i];
+      let move = checkCell(node, figureWhite, i, y);
+      if (isField(i, node, move, canMove) === null) break;
+      canMove[move.position] = move;
+    }
+    for (let i = y + 1; i < 8; i ++) {
+      if (isField(i) === null) break;
+      let node = nodeList[i][x];
+      let move = checkCell(node, figureWhite, x, i);
+      if (isField(i, node, move, canMove) === null) break;
+      canMove[move.position] = move;
+    }
+    for (let i = y - 1; i < y; i --) {
+      if (isField(i) === null) break;
+      let node = nodeList[i][x];
+      let move = checkCell(node, figureWhite, x, i);
+      if (isField(i, node, move, canMove) === null) break;
+      canMove[move.position] = move;
+    }
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
+    return canMove;
+  }
 
+  function king(name, x, y, figureWhite, nodeList) {
+    let canMove = {};
+    for (let i = y - 1; i <= y + 1; i++) {
+      for (let k = x - 1; k <= x + 1; k++) {
+        if (isField(i) !== null && isField(k) !== null) {
+          let node = nodeList[i][k];
+          let move = checkCell(node, figureWhite, k, i);
+          if (isField(i, node, move, canMove) !== null) canMove[move.position] = move;
+        }
+      }
+    }
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
+    return canMove;
+  }
 
-const Chess = () => {
+  function bishop(name, x, y, figureWhite, nodeList) {
+    let canMove = {};
+    let diagonal = {
+      left: true,
+      right: true,
+      minusLeft: true,
+      minusRight: true
+    }
+    for (let i = 1; i < 8; i++) {
+      if (isField(x + i) === null || isField(y + i) === null) diagonal.right = false;
+      if (isField(x - i) === null || isField(y + i) === null) diagonal.left = false;
+      if (isField(x + i) === null || isField(y - i) === null) diagonal.minusRight = false;
+      if (isField(x - i) === null || isField(y - i) === null) diagonal.minusLeft = false;
+      if (diagonal.right) {
+        let node = nodeList[y + i][x + i];
+        let move = checkCell(node, figureWhite, x + i, y + i);
+        if (isField(1, node, move, canMove) !== null) canMove[move.position] = move;
+        else diagonal.right = false;
+      }
+      if (diagonal.left) {
+        let node = nodeList[y + i][x - i];
+        let move = checkCell(node, figureWhite, x - i, y + i);
+        if (isField(1, node, move, canMove) !== null) canMove[move.position] = move;
+        else diagonal.left = false;
+      }
+      if (diagonal.minusRight) {
+        let node = nodeList[y - i][x + i];
+        let move = checkCell(node, figureWhite, x + i, y - i);
+        if (isField(1, node, move, canMove) !== null) canMove[move.position] = move;
+        else diagonal.minusRight = false;
+      }
+      if (diagonal.minusLeft) {
+        let node = nodeList[y - i][x - i];
+        let move = checkCell(node, figureWhite, x - i, y - i);
+        if (isField(1, node, move, canMove) !== null) canMove[move.position] = move;
+        else diagonal.minusLeft = false;
+      }
+    }
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
+    return canMove;
+  }
+
+  function knight(name, x, y, figureWhite, nodeList) {
+    let canMove = {};
+    let count = 0;
+    for (let i = x - 2; i <= x + 2; i++) {
+      if (i !== x) {
+        if (isField(i) !== null) {
+          if (count %2 === 0) {
+            if (isField(y + 1) !== null) {
+              let node = nodeList[y + 1][i];
+              let move = checkCell(node, figureWhite, i, y + 1);
+              if (isField(i, node, move, canMove) !== null) canMove[move.position] = move;
+            }
+            if (isField(y - 1) !== null) {
+              let node = nodeList[y - 1][i];
+              let move = checkCell(node, figureWhite, i, y - 1);
+              if (isField(i, node, move, canMove) !== null) canMove[move.position] = move;
+            }
+          } else {
+            if (isField(y + 2) !== null) {
+              let node = nodeList[y + 2][i];
+              let move = checkCell(node, figureWhite, i, y + 2);
+              if (isField(i, node, move, canMove) !== null) canMove[move.position] = move;
+            }
+            if (isField(y - 2) !== null) {
+              let node = nodeList[y - 2][i];
+              let move = checkCell(node, figureWhite, i, y - 2);
+              if (isField(i, node, move, canMove) !== null) canMove[move.position] = move;
+            }
+          }
+        }
+      }
+      count ++;
+    }
+    if (Object.keys(canMove) < 1) return {move: null, position: null};
+    return canMove;
+  }
+
+  const checkCell = (node, comparableFigureColor, x, y) => {
+    const {position} = node.dataset;
+    const isFigure = node.innerHTML !== '';
+    const currentFigureColor = node.childNodes.length > 0 ? node.childNodes[0].children[0].dataset.name.includes('White') : null;
+    if (!isFigure) return {move: 'move', position, x, y};
+    if (isFigure && comparableFigureColor !== currentFigureColor) return {move: 'attack', position, x: x, y: y};
+    if (isFigure && comparableFigureColor === currentFigureColor) return {move: null}
+    return {};
+  }
+
+  function isField(i, node, move, canMove) {
+    let pos = !(i < 0 || i >= 8);
+    if (!pos) return null;
+    move = move || {move: ''};
+    if (move.move === null) return null;
+    if (move.move === 'attack') {
+      canMove[move.position] = move;
+      return null;
+    }
+  }
+
   const [figures, setFigures] = useState(figuresList);
   const [nodeCells, setNodeCells] = useState([]);
   const [nodeCellsReverse, setNodeCellsReverse] = useState([]);
@@ -147,7 +300,7 @@ const Chess = () => {
       const whiteFigure = name.includes('White') || false;
       if (name !== 'cell' && !isCurrentFigure && whiteFigure === whiteMove) {
         const figureWhite = name.includes('White');
-        const canMove = figures[name].move(name, parseInt(x), parseInt(y), figureWhite, nodeList);
+        const canMove = figures[name].move(name, parseInt(x), parseInt(y), figureWhite, nodeList, figuresList);
         if (canMove.move === null) {
           setCurrentFigure({});
           return null
